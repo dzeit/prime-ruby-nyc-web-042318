05f6b237eb018748917ed2bf9ab95916a68aaf8b
David Zeit <dzeit216@gmail.com>
2018-04-17 15:58:32 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,13 @@ def prime?(number)
   if number <= 1
     return false
   end
+
   if number % 2 == 0 && number != 2
     return false
   end
-  (3..(number / 2)).to_a.each do |n|
+
+  range = (3..(number / 2)).to_a
+  range.each do |n|
     if number % n == 0
       return false
     end
